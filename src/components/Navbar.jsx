65621dd8532c19d78c1a658c1194bb410d7148da
Emrah Bayar <emrah.bayar.98@gmail.com>
2025-06-22 00:13:53 +0300

    Mobil görünüm için düzenlemeler yapıldı.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ function Navbar() {
         <>
             <nav className="navbar navbar-expand-lg navbar-container">
                 <div className="container-fluid">
-                    <a className="navbar-brand nav-header">Move Text</a>
+                    <Link className='nav-header ' to='/'>Move Text</Link>
                     <button className="navbar-toggler toggler-button" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon "></span>
                     </button>
